91f36c39544a06f3ded2af747bd08ed4379f161e
hpcys2496393489@qq.com <hpcys2496393489@qq.com>
2020-06-12 20:58:28 +0800

    修改后的

--- run_suite.py
+++ run_suite.py
@@ -8,6 +8,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 suite = unittest.TestLoader().discover(BASE_DIR + '/script', 'test*.py')
 
-file = 'ihrm{}.html'.format(time.strftime('%Y%m%d%H%M%S'))
-
+# file = 'ihrm{}.html'.format(time.strftime('%Y%m%d%H%M%S'))
+file = "ihrm{}.html"
 BeautifulReport(suite).report(filename=file, description='部门管理模块', log_path=BASE_DIR + '/report')
